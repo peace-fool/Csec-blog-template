7f803389d775a90d74373099ac3acbd02a88c2e6
peace-fool <utkarsh.c137@gmail.com>
2021-09-13 15:32:26 +0530

    Initial Commit

---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+  <title>test</title>
+</head>
+<body>
+<?php include 'nav/sidebar.php';?>
+<!-- Main Wrapper -->
+<div class="my-container active-cont">
+  <!-- Top Nav -->
+  <nav class="navbar sticky-top navbar-light bg-light px-5">
+    <a class="btn border-0" id="menu-btn"><i class="bx bx-menu"></i></a>
+  </nav>
+  <!--End Top Nav -->
+  
+  <div class="blog">
+<!-- Content start -->
+
+    
+
+<!-- Content end -->
+  </div>
+</div>
+<?php include 'nav/side_script.php';?>
+</body>
+</html>
